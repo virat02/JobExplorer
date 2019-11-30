9e725b7b0b5261c692af87eb611db6873532f278
virat02 <virat.goradia96@gmail.com>
2019-11-30 01:27:40 -0500

    Simple hello world app running

---
+++ BackEnd/src/backend-jobExplorer/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\virat\desktop\jobexplorer\backend\backend-jobexplorer\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
