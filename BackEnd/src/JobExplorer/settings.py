acf01d8e88039bd3231670a37a0d33159ee545cf
virat02 <virat.goradia96@gmail.com>
2019-12-06 13:45:10 -0500

    Filter based searching added in api

--- BackEnd/src/JobExplorer/settings.py
+++ BackEnd/src/JobExplorer/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'jobs'
+    'jobs',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
--- BackEnd/src/jobs/api/views.py
+++ BackEnd/src/jobs/api/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
+from django_filters import rest_framework as filters
+from rest_framework.filters import SearchFilter
 
 from jobs.models import Job
 from .serializers import JobSerializer
 
 
 class JobViewSet(viewsets.ModelViewSet):
+    __basic_fields = ('language', 'sponsorship_available', 'type', 'location')
     serializer_class = JobSerializer
     queryset = Job.objects.all()
+    filter_backends = [SearchFilter, filters.DjangoFilterBackend]
+    # search_fields = ['language', 'sponsorship_available', 'type', 'location']
+    filter_fields = __basic_fields
+    search_fields = __basic_fields
